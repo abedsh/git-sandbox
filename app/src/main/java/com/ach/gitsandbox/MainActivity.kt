d94492eb7d3ee845e453e10776856728be6cef19
Abed shmaytilli <abed.chmaitelli@gmail.com>
2022-06-01 20:56:30 -0400

    comment for feature 4

--- app/src/main/java/com/ach/gitsandbox/MainActivity.kt
+++ app/src/main/java/com/ach/gitsandbox/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        
+        // this is a comment for feature 4 to see how rebasing to an updated dev works
     }
 }
