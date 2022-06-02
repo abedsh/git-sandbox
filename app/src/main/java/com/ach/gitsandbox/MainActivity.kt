6f1eb2ccca9226a9be321351ffdccba2ad5c71d1
Abed shmaytilli <abed.chmaitelli@gmail.com>
2022-06-01 20:57:47 -0400

    comment for feature 1

--- app/src/main/java/com/ach/gitsandbox/MainActivity.kt
+++ app/src/main/java/com/ach/gitsandbox/MainActivity.kt
@@ -7,5 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // this is a comment for feature 1 to merge before feature 3 or 4
+
     }
 }
